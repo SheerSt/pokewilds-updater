d0e9747c7ca2cb9d03082e365f05191b54a8b4f7
Tarkan Nielsen <pmtarkan@gmail.com>
2022-06-19 19:35:12 +0200

    Added application plugin to make the final jar launchable

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
+version = "1.0.0"
 group = "com.pkmngen.updater"
 
 plugins {
     java
+    application
 
     id("com.github.johnrengelman.shadow") version "7.1.1"
+}
+
+application {
+    mainClass.set("com.pkmngen.updater.UpdaterApplication")
 }
 
 repositories {
